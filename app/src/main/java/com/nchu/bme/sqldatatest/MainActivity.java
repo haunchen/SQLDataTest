9e987c6d008a14418e81e825fe32d99f503fb252
haunchen <35416167+haunchen@users.noreply.github.com>
2019-11-26 21:13:09 +0800

    Initial commit

---
+++ app/src/main/java/com/nchu/bme/sqldatatest/MainActivity.java
@@ -0,0 +1,159 @@
+package com.nchu.bme.sqldatatest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+public class MainActivity extends AppCompatActivity {
+
+    LineChart mChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mChart = findViewById(R.id.chart_line);
+
+        Description description = new Description();
+        description.setText("SQL Data Test");
+
+        mChart.setDescription(description);
+
+        mChart.setTouchEnabled(true);
+
+        // 可拖曳
+        mChart.setDragEnabled(true);
+
+        // 可缩放
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        mChart.setPinchZoom(true);
+
+        // 设置图表的背景颜色
+        mChart.setBackgroundColor(Color.LTGRAY);
+
+        LineData data = new LineData();
+
+        // 数据显示的颜色
+        data.setValueTextColor(Color.WHITE);
+
+        // 先增加一个空的数据，随后往里面动态添加
+        mChart.setData(data);
+
+        // 图表的注解(只有当数据集存在时候才生效)
+        Legend l = mChart.getLegend();
+
+        // 可以修改图表注解部分的位置
+        // l.setPosition(LegendPosition.LEFT_OF_CHART);
+
+        // 线性，也可是圆
+        l.setForm(Legend.LegendForm.LINE);
+
+        // 颜色
+        l.setTextColor(Color.WHITE);
+
+        // x坐标轴
+        XAxis xl = mChart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(false);
+        xl.setAvoidFirstLastClipping(true);
+
+        // 几个x坐标轴之间才绘制？
+        //xl.setSpaceBetweenLabels(5);
+
+        // 如果false，那么x坐标轴将不可见
+        xl.setEnabled(true);
+
+        // 将X坐标轴放置在底部，默认是在顶部。
+        xl.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        // 图表左边的y坐标轴线
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTextColor(Color.WHITE);
+
+        // 最大值
+        leftAxis.setAxisMaxValue(90f);
+
+        // 最小值
+        leftAxis.setAxisMinValue(40f);
+
+        // 不一定要从0开始
+        leftAxis.setStartAtZero(false);
+
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        // 不显示图表的右边y坐标轴线
+        rightAxis.setEnabled(false);
+
+
+
+        // 每点击一次按钮，增加一个点
+        Button addButton = findViewById(R.id.button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                addEntry();
+            }
+        });
+
+    }
+    public void addEntry(){
+        LineData data = mChart.getLineData();
+
+        LineDataSet dataSet = (LineDataSet)data.getDataSetByIndex(0);
+
+        if (dataSet==null){
+            dataSet = createLineDataSet();
+            data.addDataSet(dataSet);
+        }
+
+        Entry entry=new Entry((float)(Math.random() * 20 + 1), dataSet.getEntryCount());
+
+        data.addEntry(entry,0);
+
+        mChart.notifyDataSetChanged();
+
+        // 當前統計圖表中最多在x軸座標線上顯示的總量
+        mChart.setVisibleXRangeMaximum(10);
+        // 將座標移動到最新
+        // 此代碼將刷新圖表的繪圖
+        mChart.moveViewToX(data.getXMax() - 5);
+    }
+    private LineDataSet createLineDataSet() {
+
+        LineDataSet set = new LineDataSet(null, "动态添加的数据");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        // 折线的颜色
+        set.setColor(ColorTemplate.getHoloBlue());
+
+        set.setCircleColor(Color.WHITE);
+        set.setLineWidth(10f);
+        set.setCircleSize(5f);
+        set.setFillAlpha(128);
+        set.setFillColor(ColorTemplate.getHoloBlue());
+        set.setHighLightColor(Color.GREEN);
+        set.setValueTextColor(Color.WHITE);
+        set.setValueTextSize(10f);
+        set.setDrawValues(true);
+        return set;
+    }
+}
